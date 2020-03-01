42e11400449f071c8e118a7c7aab7b4ccccfb5ad
py <zhaopy4721@outlook.com>
2020-03-01 20:42:17 +0800

    Add the app list lib

--- qq_app/qq_app.go
+++ qq_app/qq_app.go
@@ -1,127 +1,83 @@
-package pholcus_list
+package pholcus_lib
 
 // 基础包
 import (
-	"log"
-
-	// "github.com/henrylee2cn/pholcus/common/goquery"                        //DOM解析
+	// "github.com/henrylee2cn/pholcus/common/goquery"                          //DOM解析
 	"github.com/henrylee2cn/pholcus/app/downloader/request" //必需
-	// "github.com/henrylee2cn/pholcus/logs"               //信息输出
-	. "github.com/henrylee2cn/pholcus/app/spider" //必需
-	// . "github.com/henrylee2cn/pholcus/app/spider/common" //选用
+	. "github.com/henrylee2cn/pholcus/app/spider"           //必需
 
+	// . "github.com/henrylee2cn/pholcus/app/spider/common" //选用
+	// "github.com/henrylee2cn/pholcus/logs"
 	// net包
 	// "net/http" //设置http.Header
 	// "net/url"
-
 	// 编码包
-
 	// "encoding/xml"
-	"encoding/json"
+	//"encoding/json"
 	// 字符串处理包
-	// "regexp"
+	//"regexp"
 	// "strconv"
-	// "strings"
-	// 其他包
 	// "fmt"
 	// "math"
 	// "time"
+	"strings"
+
+	"github.com/henrylee2cn/pholcus/common/goquery"
 )
 
 func init() {
-	App.Register()
+	FileTest.Register()
 }
 
-type Item struct {
-	Id       string `json:"id"`
-	Title    string `json:"title"`
-	Url      string `json:"url"`
-	Date     string `json:"date"`
-	NodeId   string `json:"nodeId"`
-	ImgCount string `json:"imgCount"`
-}
-type News struct {
-	Items []Item `json:"items"`
-}
-
-var news News
-
-var App = &Spider{
+var FileTest = &Spider{
 	Name:        "应用宝",
 	Description: "抓取应用宝应用列表",
-	// Pausetime:    300,
-	// Keyin:        KEYIN,
+	// Pausetime: 300,
+	// Keyin:   KEYIN,
 	// Limit:        LIMIT,
 	EnableCookie: false,
 	RuleTree: &RuleTree{
 		Root: func(ctx *Context) {
 			ctx.AddQueue(&request.Request{
-				Method: "GET",
-				Url:    "https://sj.qq.com/myapp/category.htm?orgame=1",
-				Rule:   "应用列表",
+				Url:  "https://sj.qq.com/myapp/category.htm?orgame=1",
+				Rule: "应用列表",
 			})
 		},
 
 		Trunk: map[string]*Rule{
+
 			"应用列表": {
 				ParseFunc: func(ctx *Context) {
-
-					//query := ctx.GetDom()
-					//str := query.Find("body").Text()
-
-					//str := `{"items":[{"id":"282","title":"人社&nbsp;转型升级&quot;战术&quot;手册","url":"ht","date":"201","nodeId":"1001","imgCount":"4"}]}`
-
-					str := ctx.GetText()
-
-					err := json.Unmarshal([]byte(str), &news)
-					if err != nil {
-						log.Printf("解析错误： %v\n", err)
-						return
-					}
-					/////////////////
-					newsLength := len(news.Items)
-					for i := 0; i < newsLength; i++ {
-						ctx.AddQueue(&request.Request{
-							Url:  news.Items[i].Url,
-							Rule: "热点新闻",
-							Temp: map[string]interface{}{
-								"id":       news.Items[i].Id,
-								"title":    news.Items[i].Title,
-								"date":     news.Items[i].Date,
-								"newsType": news.Items[i].NodeId,
-							},
+					ItemFields: []string{
+						"名称",
+						"大小",
+						"下载量",
+						"链接",
+					},
+	
+					ParseFunc: func(ctx *Context) {
+						query := ctx.GetDom()
+						//获取应用列表
+						newList := query.Find(".main li")
+						newList.Each(func(i int, s *goquery.Selection) {
+							//名称
+							appTitle:= s.Find(".app-info-desc a").Text()
+							//大小
+							appSize := s.Find(".size").Text()
+							//下载量
+							appDownload := s.Find(".download").Text()
+							//链接
+							url, _ := s.Find(".app-info-desc a").Attr("href")
+	
+							//输出格式
+							ctx.Output(map[int]interface{}{
+								0: appTitle,
+								1: appSize,
+								2: appDownload,
+								3: url,
+							})
 						})
-					}
-					/////////////////
-				},
-			},
-
-			"获取信息": {
-				//注意：有无字段语义和是否输出数据必须保持一致
-				ItemFields: []string{
-					"ID",
-					"标题",
-					"内容",
-					"类别",
-					"ReleaseTime",
-				},
-				ParseFunc: func(ctx *Context) {
-					query := ctx.GetDom()
-
-					// 获取内容
-					content := query.Find("#p_content").Text()
-					// re, _ := regexp.Compile("\\<[\\S\\s]+?\\>")
-					// content = re.ReplaceAllStringFunc(content, strings.ToLower)
-					// content = re.ReplaceAllString(content, "")
-
-					// 结果存入Response中转
-					ctx.Output(map[int]interface{}{
-						0: ctx.GetTemp("id", ""),
-						1: ctx.GetTemp("title", ""),
-						2: content,
-						3: ctx.GetTemp("newsType", ""),
-						4: ctx.GetTemp("date", ""),
-					})
+					},
 				},
 			},
 		},
